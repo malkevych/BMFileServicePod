e000f2ea8e9256e39ecc0bb75ecb6884ca64bd94
Malkevych Bohdan Ihorovych <Bohdan@MacBook-Pro-oivrafiv-2.local>
2016-07-18 16:13:31 +0300

    changed Dorpbox imports

--- Classes/dropBox/ItemDropServiceFile.h
+++ Classes/dropBox/ItemDropServiceFile.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "ItemFromFileServiceProtocol.h"
 #import "ItemDriveServiceFile.h"
-#import "Dropbox-iOS-SDK/DropboxSDK.h"
+#import "DropboxSDK.h"
 
 @interface ItemDropServiceFile : DBMetadata <ItemFromFileServiceProtocol>
 
